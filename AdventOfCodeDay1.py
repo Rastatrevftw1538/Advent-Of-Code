b80ed522beb62fcc68b0cec6c6f5270bd5718b00
Rastatrevftw1538 <trevor.cardoza1538@gmail.com>
2019-12-01 15:56:46 -0800

    Advent Of Code Day One Complete

--- AdventOfCodeDay1.py
+++ AdventOfCodeDay1.py
@@ -1,4 +1,28 @@
 import math
+import re
 
 
-class main(Input):
+def main(Input):
+    read = open(Input, 'r')
+    mass = []
+    totalFuelValue = 0
+    regex = re.compile("([\d]+)")
+    massList = read.read()
+
+    for thing in regex.finditer(massList):
+        mass.append(thing.group(1))
+
+    for i in mass:
+        totalFuelForModule = 0
+        fuel_equation = math.floor(int(i) / 3) - 2
+        fuel_num = fuel_equation
+        totalFuelForModule += fuel_equation
+        while fuel_num > 0:
+            fuel_num = math.floor(int(fuel_num) / 3) - 2
+            if fuel_num > 0:
+                totalFuelForModule += fuel_num
+        totalFuelValue += totalFuelForModule
+    return(print(totalFuelValue))
+
+
+main("MassList.txt")
