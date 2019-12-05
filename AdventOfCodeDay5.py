67b263e0bc0e46229ba9a2d4a97a637c7162cf41
Rastatrevftw1538 <trevor.cardoza1538@gmail.com>
2019-12-05 11:18:42 -0800

    Part 2

--- AdventOfCodeDay5.py
+++ AdventOfCodeDay5.py
@@ -4,44 +4,44 @@ def get_modes(modes):
     return [int(mode) for mode in [modes[2], modes[1], modes[0], modes[3:]]]
 
 
-def get_param(mode, increment, idx, data):
+def get_param(mode, increment, index, data):
     if mode == 0:
-        return data[data[idx + increment]]
-    return data[idx + increment]
+        return data[data[index + increment]]
+    return data[index + increment]
 
 
-def get_params(mode1, mode2, idx, data):
-    return get_param(mode1, 1, idx, data), get_param(mode2, 2, idx, data)
+def get_params(mode1, mode2, index, data):
+    return get_param(mode1, 1, index, data), get_param(mode2, 2, index, data)
 
 
-def add(mode1, mode2, idx, data):
-    param1, param2 = get_params(mode1, mode2, idx, data)
-    data[data[idx + 3]] = param1 + param2
+def add(mode1, mode2, index, data):
+    param1, param2 = get_params(mode1, mode2, index, data)
+    data[data[index + 3]] = param1 + param2
 
 
-def multiply(mode1, mode2, idx, data):
-    param1, param2 = get_params(mode1, mode2, idx, data)
-    data[data[idx + 3]] = param1 * param2
+def multiply(mode1, mode2, index, data):
+    param1, param2 = get_params(mode1, mode2, index, data)
+    data[data[index + 3]] = param1 * param2
 
 
-def less_than(mode1, mode2, idx, data):
-    param1, param2 = get_params(mode1, mode2, idx, data)
-    data[data[idx + 3]] = 1 if param1 < param2 else 0
+def less_than(mode1, mode2, index, data):
+    param1, param2 = get_params(mode1, mode2, index, data)
+    data[data[index + 3]] = 1 if param1 < param2 else 0
 
 
-def equals(mode1, mode2, idx, data):
-    param1, param2 = get_params(mode1, mode2, idx, data)
-    data[data[idx + 3]] = 1 if param1 == param2 else 0
+def equals(mode1, mode2, index, data):
+    param1, param2 = get_params(mode1, mode2, index, data)
+    data[data[index + 3]] = 1 if param1 == param2 else 0
 
 
-def jump_if_true(mode1, mode2, idx, data):
-    param1, param2 = get_params(mode1, mode2, idx, data)
-    return param2 if param1 != 0 else idx + 3
+def jump_if_true(mode1, mode2, index, data):
+    param1, param2 = get_params(mode1, mode2, index, data)
+    return param2 if param1 != 0 else index + 3
 
 
-def jump_if_false(mode1, mode2, idx, data):
-    param1, param2 = get_params(mode1, mode2, idx, data)
-    return param2 if param1 == 0 else idx + 3
+def jump_if_false(mode1, mode2, index, data):
+    param1, param2 = get_params(mode1, mode2, index, data)
+    return param2 if param1 == 0 else index + 3
 
 
 def main(data, inputs):
@@ -61,10 +61,21 @@ def main(data, inputs):
         elif opcode == 4:
             diagnostic = data[data[index + 1]]
             index += 2
+        elif opcode == 5:
+            index = jump_if_true(mode1, mode2, index, data)
+        elif opcode == 6:
+            index = jump_if_false(mode1, mode2, index, data)
+        elif opcode == 7:
+            less_than(mode1, mode2, index, data)
+            index += 4
+        elif opcode == 8:
+            equals(mode1, mode2, index, data)
+            index += 4
     return diagnostic
 
 
 with open("CodeListNewParam.txt", 'r') as CodeList:
     for thing in CodeList:
         inputs = [int(val) for val in thing.split(",")]
-        print("Answer: ", main(inputs[:], 1))
+        print("Answer Part 1: ", main(inputs[:], 1))
+        print("Answer Part 2: ", main(inputs[:], 5))
